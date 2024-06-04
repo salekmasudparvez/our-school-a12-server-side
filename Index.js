7caed572d958b4f2a5b6f68b25b4796612877a04
Salek Masud Parvez <151811650+salekmasudparvez@users.noreply.github.com>
2024-06-04 12:16:23 +0600

    added functionality about reviews

--- Index.js
+++ Index.js
@@ -6,9 +6,7 @@ const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const corsOptions = {
-  origin: [
-    "http://localhost:5173",
-  ],
+  origin: ["http://localhost:5173"],
   credentials: true,
   optionSuccessStatus: 200,
 };
@@ -32,39 +30,81 @@ async function run() {
     // await client.connect();
     // Send a ping to confirm a successful connection
 
-    const db = client.db('OurSchoolDB')
-    const usersCollection = db.collection('users')
-    const sessionCollection = db.collection('sessions')
+    const db = client.db("OurSchoolDB");
+    const usersCollection = db.collection("users");
+    const sessionCollection = db.collection("sessions");
+    const bookedSessionCollection = db.collection("bookedSessions");
+    const reviewsCollection = db.collection("reviews");
 
     //handle users collection
-     app.post('/users', async(req,res)=>{
-        const newUser = req.body;
-        console.log(newUser)
-        const result = await usersCollection.insertOne(newUser);
-        res.send(result)
-     })
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+      // console.log(newUser);
+      const result = await usersCollection.insertOne(newUser);
+      res.send(result);
+    });
 
-     app.get('/users/:email', async(req,res)=>{
-        const currentEmail = req.params.email
-        console.log(currentEmail)
-        const singleUser = await usersCollection.findOne({email:currentEmail});
-        res.send(singleUser)
-     })
+    app.get("/users/:email", async (req, res) => {
+      const currentEmail = req.params.email;
+      // console.log(currentEmail);
+      const singleUser = await usersCollection.findOne({ email: currentEmail });
+      res.send(singleUser);
+    });
 
-     //session data part
-     app.get('/sessions', async (req, res) => {
+    //session data part
+    app.get("/sessions", async (req, res) => {
       const allSessions = await sessionCollection.find({}).toArray();
       res.send(allSessions);
-     })
-     app.get('/sessions/:id', async (req, res) => {
+    });
+    app.get("/sessions/:id", async (req, res) => {
       const getID = req.params.id;
-      const query =  {
-        _id:new ObjectId(getID),
-      }
-      console.log(query)
-      const allSessions = await sessionCollection.find(query).toArray();
+      const query = {
+        _id: new ObjectId(getID),
+      };
+      const allSessions = await sessionCollection.findOne(query);
       res.send(allSessions);
-     })
+    });
+
+    //BOOK session FROM STUDENT from session details
+
+    app.get("/bookedsessions", async (req, res) => {
+      const getEmail = req.query.email;
+      const getId = req.query.id;
+      const query = {
+        BookId: getId,
+        studentEmail: getEmail,
+      };
+      // console.log(query);
+      const singleSession = await bookedSessionCollection.findOne(query);
+      res.send(!!singleSession);
+    });
+    app.get("/bookedsessiontable/:email", async (req, res) => {
+      const email = req.params.email;
+      const allSessions = await bookedSessionCollection.find({
+        studentEmail:email}).toArray();
+      res.send(allSessions);
+    });
+
+    app.post("/bookedsession", async (req, res) => {
+      const newSession = req.body;
+      // console.log(newSession);
+      const result = await bookedSessionCollection.insertOne(newSession);
+      res.send(result);
+    });
+    //get reviews and post review
+    app.get("/reviews/:id", async (req, res) => {
+      const id= req.params.id;
+      console.log(id,'line97')
+      const allReviews = await reviewsCollection.find({reviewId:id}).toArray();
+      res.send(allReviews);
+    });
+    app.post("/reviews", async (req, res) => {
+      const newReview = req.body;
+      console.log(newReview);
+      const result = await reviewsCollection.insertOne(newReview);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
